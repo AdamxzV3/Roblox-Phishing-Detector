4d3f20702c1b27ed789248e4b1f613beb7b8e1f2
Wsg <99908796+AdamxzV3@users.noreply.github.com>
2023-03-14 09:01:48 +0100

    Update and rename Detector.bat to PhishingDetector.bat

---
+++ PhishingDetector.bat
@@ -0,0 +1,40 @@
+@echo off
+
+:start
+set /p url="Enter a URL: "
+
+set "main_url=www.roblox.com"
+set "second_url=web.roblox.com"
+
+for /f "tokens=1 delims=/" %%a in ("%url%") do (
+  set "hostname=%%a"
+)
+
+set "private_server_string=privateServerLinkCode="
+
+if /i "%hostname%"=="%main_url%" (
+  color 2
+  echo Not a phishing
+) else if /i "%hostname%"=="%second_url%" (
+  color 2
+  echo Not a phishing
+) else if not "%url:~-33%"=="%private_server_string%" (
+  color 4
+  echo Phishing detected
+) else (
+  set "psl=%url:*%private_server_string%=privateServerLinkCode=%"
+  set "psl=%psl:~0,-33%"
+  set "new_url=%url:privateServerLinkCode=%psl%"
+  if not "%new_url%"=="%url%" (
+    color 2
+    echo Not a phishing
+  ) else (
+    color 4
+    echo Phishing detected
+  )
+)
+
+pause
+cls
+color 7
+goto start
